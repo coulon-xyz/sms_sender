6302559a11fbe5d27a8b238590832ef7e9c95c26
jc <j@c.com>
2018-03-24 23:12:26 +0100

    adds import random

--- send_sms.py
+++ send_sms.py
@@ -3,14 +3,15 @@ import nexmo
 
 from config import config
 
-client = nexmo.Client(key=config.NEXMO_KEY, secret=config.NEXMO_SECRET)
+if __name__ == "__main__":
+    client = nexmo.Client(key=config.NEXMO_KEY, secret=config.NEXMO_SECRET)
 
-remote_number = config.REMOTE_PHONE_NUMBER
+    remote_number = config.REMOTE_PHONE_NUMBER
 
-r = client.send_message({
-    'from': 'GoldenBoot{}'.format(random.randint(1, 100)),
-    'to': remote_number,
-    'text': 'David est en ligne et te propose un duel !',
-})
+    r = client.send_message({
+        'from': 'GoldenB{}'.format(random.randint(1, 100)),
+        'to': remote_number,
+        'text': 'David est en ligne et te propose un duel !',
+    })
 
-print(r)
+    print(r)
