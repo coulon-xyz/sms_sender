797077b9889cae5da4f28960743944217ff616e3
jc <j@c.com>
2018-03-27 21:05:40 +0200

    V2

--- send_sms.py
+++ send_sms.py
@@ -1,17 +1,26 @@
 import random
 import nexmo
+import pandas as pd
+import os
 
 from config import config
 
 if __name__ == "__main__":
-    client = nexmo.Client(key=config.NEXMO_KEY, secret=config.NEXMO_SECRET)
+    cwd = os.getcwd()
+    db = pd.read_csv(os.path.join(cwd, 'data', 'db_names.csv'))
+    dice_number = random.randint(1, 6)
 
-    remote_number = config.REMOTE_PHONE_NUMBER
+    print(dice_number)
 
-    r = client.send_message({
-        'from': 'GoldenB{}'.format(random.randint(1, 100)),
-        'to': remote_number,
-        'text': 'David est en ligne et te propose un duel !',
-    })
+    if dice_number == 1:
+        client = nexmo.Client(key=config.NEXMO_KEY, secret=config.NEXMO_SECRET)
 
-    print(r)
+        remote_number = config.REMOTE_PHONE_NUMBER
+        firstname = db['firstname'][random.randint(1, len(db))]
+        r = client.send_message({
+            'from': firstname,
+            'to': remote_number,
+            'text': '{} est en ligne et te propose un duel !'.format(firstname),
+        })
+
+        print(r)
